decc73166df4237c337b3965caa78b340b6e16c2
Vadim Vatrya <VatryaVV@gmail.com>
2022-04-18 15:52:36 +0300

    mybutton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 // import Counter from './components/Counter';
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 import './App.css';
+import MyButton from './components/UI/MyButton';
 
 function App() {
-  // const [value, setValue] = useState('Текст в инпуте');
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'Java', body: 'Description' },
+    { id: 3, title: 'Python', body: 'Description' },
+  ]);
 
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton>Создать пост</MyButton>
+      </form>
+
+      <PostList posts={posts} title="Список постов" />
+
       {/* <Counter /> */}
-      <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
     </div>
   );
 }
---
+++ src/components/UI/MyButton/MyButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './MyButton.module.scss';
+
+const MyButton = props => {
+  return <button className={styles.myBtn}></button>;
+};
+
+export default MyButton;
