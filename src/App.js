4391d20fe382ca133d14ed91a776c2d0b6561b55
Vadim Vatrya <VatryaVV@gmail.com>
2022-04-19 23:58:23 +0300

    select

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 // import Counter from './components/Counter';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 import './App.css';
 
@@ -12,18 +13,50 @@ function App() {
     { id: 3, title: 'Python', body: 'Description' },
   ]);
 
+  const [selectedSort, setSelectedSort] = useState('');
+
   const createPost = newPost => {
     setPosts([...posts, newPost]);
   };
 
   const removePost = post => {
-    posts.filter(p => p.id !== post.id);
+    setPosts(posts.filter(p => p.id !== post.id));
+  };
+
+  const sortPost = sort => {
+    setSelectedSort(sort);
+    console.log(sort);
   };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Список постов" />
+      <hr className="delimiter"></hr>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPost}
+          defaultValue="Сортировка"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
+      {/* <div>
+        <MySelect
+          defaultValue="Cортировка"
+          option={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div> */}
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Список постов" />
+      ) : (
+        <h1 className="main"> Posts not found!</h1>
+      )}
 
       {/* <Counter /> */}
     </div>
