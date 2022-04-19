5d0608f773e70b36c7904ca7d3879e4a684786b6
Vadim Vatrya <VatryaVV@gmail.com>
2022-04-20 00:34:02 +0300

    search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 // import Counter from './components/Counter';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+
 import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 import './App.css';
 
@@ -14,6 +16,19 @@ function App() {
   ]);
 
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function getSortedPosts() {
+    console.log('ОТРАБОТАЛА ФУНКЦИЯ getSortedPosts');
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort]),
+      );
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts();
 
   const createPost = newPost => {
     setPosts([...posts, newPost]);
@@ -25,7 +40,6 @@ function App() {
 
   const sortPost = sort => {
     setSelectedSort(sort);
-    console.log(sort);
   };
 
   return (
@@ -33,6 +47,11 @@ function App() {
       <PostForm create={createPost} />
       <hr className="delimiter"></hr>
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={event => setSearchQuery(event.target.value)}
+          placeholder="Поиск..."
+        />
         <MySelect
           value={selectedSort}
           onChange={sortPost}
@@ -43,17 +62,12 @@ function App() {
           ]}
         />
       </div>
-      {/* <div>
-        <MySelect
-          defaultValue="Cортировка"
-          option={[
-            { value: 'title', name: 'По названию' },
-            { value: 'body', name: 'По описанию' },
-          ]}
-        />
-      </div> */}
       {posts.length ? (
-        <PostList remove={removePost} posts={posts} title="Список постов" />
+        <PostList
+          remove={removePost}
+          posts={sortedPosts}
+          title="Список постов"
+        />
       ) : (
         <h1 className="main"> Posts not found!</h1>
       )}
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -16,22 +16,3 @@ const MySelect = ({ options, defaultValue, value, onChange }) => {
 };
 
 export default MySelect;
-
-// import React from 'react';
-
-// const MySelect = ({ options, defaultValue, value, onChange }) => {
-//   return (
-//     <select>
-//       {/* <option disabled value="">
-//         {defaultValue}
-//       </option> */}
-//       {options.map(option => (
-//         <option key={option.value} value={option.value}>
-//           {option.name}
-//         </option>
-//       ))}
-//     </select>
-//   );
-// };
-
-// export default MySelect;
